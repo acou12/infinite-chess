a609334a9b3e4a4dacdb9d8c66d7a76ab094ea95
acou12 <51276518+acou12@users.noreply.github.com>
2021-09-21 18:01:04 -0600

    Initial alpha commit

---
+++ src/main.ts
@@ -0,0 +1,263 @@
+import "./style.css"
+import {
+  Piece,
+  Vector,
+  king,
+  queen,
+  bishop,
+  knight,
+  pawn,
+  rook,
+} from "./pieces"
+
+const canvas = document.querySelector("canvas")!
+const ctx = canvas.getContext("2d")!
+
+let img = new Image()
+img.src = "/images/wk.png"
+
+/**
+ * CONSIDERATION: In order to do minmax stuff (which I'd look to do at some point),
+ * it will be necessary to implement the game logic based on any arbitrary board state,
+ * not just the current board state. It might be best to move all chess logic into a
+ * seperate file with an abstract `Game` class (with a copy constructor) and just import
+ * that for both the AI logic and the actual UI logic.
+ */
+
+let pieces: Piece[] = [
+  { color: "WHITE", location: new Vector(0, 0), type: rook },
+  { color: "WHITE", location: new Vector(1, 0), type: knight },
+  { color: "WHITE", location: new Vector(2, 0), type: bishop },
+  { color: "WHITE", location: new Vector(3, 0), type: king },
+  { color: "WHITE", location: new Vector(4, 0), type: queen },
+  { color: "WHITE", location: new Vector(5, 0), type: bishop },
+  { color: "WHITE", location: new Vector(6, 0), type: knight },
+  { color: "WHITE", location: new Vector(7, 0), type: rook },
+
+  { color: "WHITE", location: new Vector(0, 1), type: pawn },
+  { color: "WHITE", location: new Vector(1, 1), type: pawn },
+  { color: "WHITE", location: new Vector(2, 1), type: pawn },
+  { color: "WHITE", location: new Vector(3, 1), type: pawn },
+  { color: "WHITE", location: new Vector(4, 1), type: pawn },
+  { color: "WHITE", location: new Vector(5, 1), type: pawn },
+  { color: "WHITE", location: new Vector(6, 1), type: pawn },
+  { color: "WHITE", location: new Vector(7, 1), type: pawn },
+
+  { color: "BLACK", location: new Vector(0, 7), type: rook },
+  { color: "BLACK", location: new Vector(1, 7), type: knight },
+  { color: "BLACK", location: new Vector(2, 7), type: bishop },
+  { color: "BLACK", location: new Vector(3, 7), type: king },
+  { color: "BLACK", location: new Vector(4, 7), type: queen },
+  { color: "BLACK", location: new Vector(5, 7), type: bishop },
+  { color: "BLACK", location: new Vector(6, 7), type: knight },
+  { color: "BLACK", location: new Vector(7, 7), type: rook },
+
+  { color: "BLACK", location: new Vector(0, 6), type: pawn },
+  { color: "BLACK", location: new Vector(1, 6), type: pawn },
+  { color: "BLACK", location: new Vector(2, 6), type: pawn },
+  { color: "BLACK", location: new Vector(3, 6), type: pawn },
+  { color: "BLACK", location: new Vector(4, 6), type: pawn },
+  { color: "BLACK", location: new Vector(5, 6), type: pawn },
+  { color: "BLACK", location: new Vector(6, 6), type: pawn },
+  { color: "BLACK", location: new Vector(7, 6), type: pawn },
+]
+
+let offsetX = 0
+let offsetY = 0
+let scale = 1
+
+function transformX(x: number) {
+  return x * scale + offsetX
+}
+
+function transformY(y: number) {
+  return y * scale + offsetY
+}
+
+function matchScale(n: number) {
+  return n * scale
+}
+
+function invTransformX(x: number) {
+  return (x - offsetX) / scale
+}
+
+function invTransformY(y: number) {
+  return (y - offsetY) / scale
+}
+
+function tileAt(x: number, y: number) {
+  return new Vector(
+    Math.floor(invTransformX(x) / tileSize),
+    Math.floor(invTransformY(y) / tileSize)
+  )
+}
+
+function pieceAt(x: number, y: number): Piece | undefined {
+  let tile = tileAt(x, y)
+  return pieces.find((p) => p.location.x === tile.x && p.location.y === tile.y)
+}
+
+function pieceAtTile(tile: Vector): Piece | undefined {
+  return pieces.find((p) => p.location.x === tile.x && p.location.y === tile.y)
+}
+
+const tileSize = 100
+
+function draw() {
+  requestAnimationFrame(draw)
+  canvas.width = canvas.clientWidth
+  canvas.height = canvas.clientHeight
+
+  ctx.fillStyle = "#34007a"
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  let minX = Math.floor((0 - offsetX) / tileSize / scale)
+  let minY = Math.floor((0 - offsetY) / tileSize / scale)
+
+  let maxX = (canvas.width - offsetX) / tileSize / scale
+  let maxY = (canvas.height - offsetY) / tileSize / scale
+
+  for (let x = minX; x < maxX; x++) {
+    for (let y = minY; y < maxY; y++) {
+      if ((x + y) % 2 == 0) {
+        ctx.fillStyle = "#7448b0"
+        ctx.fillRect(
+          transformX(x * tileSize),
+          transformY(y * tileSize),
+          matchScale(tileSize),
+          matchScale(tileSize)
+        )
+      }
+      if (pickedUp && legalMove(pickedUp, new Vector(x, y))) {
+        ctx.fillStyle = "black"
+        ctx.beginPath()
+        ctx.ellipse(
+          transformX(x * tileSize + tileSize / 2),
+          transformY(y * tileSize + tileSize / 2),
+          20,
+          20,
+          0,
+          0,
+          Math.PI * 2
+        )
+        ctx.fill()
+      }
+    }
+  }
+
+  for (let piece of pieces) {
+    if (!(pickedUp && piece == pickedUp))
+      ctx.drawImage(
+        piece.color === "WHITE" ? piece.type.whiteImage : piece.type.blackImage,
+        transformX(piece.location.x * tileSize),
+        transformY(piece.location.y * tileSize),
+        matchScale(tileSize),
+        matchScale(tileSize)
+      )
+  }
+
+  if (pickedUp) {
+    ctx.drawImage(
+      pickedUp.color === "WHITE"
+        ? pickedUp.type.whiteImage
+        : pickedUp.type.blackImage,
+      mousePos.x - matchScale(tileSize / 2),
+      mousePos.y - matchScale(tileSize / 2),
+      matchScale(tileSize),
+      matchScale(tileSize)
+    )
+  }
+
+  if (movingBoard) {
+    offsetX += mousePos.x - mousePosPrevious.x
+    offsetY += mousePos.y - mousePosPrevious.y
+  }
+  mousePosPrevious = {
+    ...mousePos,
+  }
+}
+
+interface Point {
+  x: number
+  y: number
+}
+
+let movingBoard = false
+let mousePos: Point = {
+  x: 0,
+  y: 0,
+}
+let mousePosPrevious: Point = {
+  x: 0,
+  y: 0,
+}
+
+document.addEventListener("mousemove", (e) => {
+  mousePos = {
+    x: e.clientX,
+    y: e.clientY,
+  }
+})
+
+let pickedUp: Piece | undefined = undefined
+
+document.addEventListener("mousedown", () => {
+  let piece = pieceAt(mousePos.x, mousePos.y)
+  if (piece) {
+    pickedUp = piece
+  } else {
+    movingBoard = true
+  }
+})
+
+/**
+ * @returns Whether it is legal for `piece` to move to `tile`.
+ */
+function legalMove(piece: Piece, tile: Vector) {
+  let otherPiece = pieceAtTile(tile)
+  return otherPiece
+    ? piece.type.takes(piece, tile) &&
+        !(piece === otherPiece) &&
+        !(piece.color === otherPiece.color)
+    : piece.type.moves(piece, tile)
+}
+
+/**
+ * Move a certain piece to a tile, capturing if necessary.
+ * For performance reasons, this method does not check move
+ * validity; use `legalMove` to do that.
+ */
+function movePiece(piece: Piece, tile: Vector) {
+  let otherPiece = pieceAtTile(tile)
+  if (otherPiece) pieces = pieces.filter((o) => o !== otherPiece)
+  piece.location.x = tile.x
+  piece.location.y = tile.y
+}
+
+document.addEventListener("mouseup", () => {
+  movingBoard = false
+  if (pickedUp) {
+    let tile = tileAt(mousePos.x, mousePos.y)
+    if (legalMove(pickedUp, tile)) movePiece(pickedUp, tile)
+    pickedUp = undefined
+  }
+})
+
+const scrollFactor = 0.07
+const maxScale = 5
+const minScale = 1 / 5
+
+document.addEventListener("wheel", (e) => {
+  let n = e.deltaY > 0 ? -scrollFactor : scrollFactor
+  let newScale = scale * (1 + n)
+  if (minScale < newScale && newScale < maxScale) {
+    scale *= 1 + n
+    offsetX -= (mousePos.x - offsetX) * n
+    offsetY -= (mousePos.y - offsetY) * n
+  }
+})
+
+canvas.addEventListener("contextmenu", (e) => e.preventDefault())
+
+draw()
---
+++ src/pieces.ts
@@ -0,0 +1,131 @@
+export class Vector {
+  constructor(public x: number, public y: number) {}
+  minus(other: Vector): Vector {
+    return new Vector(this.x - other.x, this.y - other.y)
+  }
+  absMinus(other: Vector): Vector {
+    return new Vector(Math.abs(this.x - other.x), Math.abs(this.y - other.y))
+  }
+  plus(other: Vector): Vector {
+    return new Vector(this.x + other.x, this.y + other.y)
+  }
+}
+
+export type Color = "WHITE" | "BLACK"
+
+/* TODO: Generate valid moves dynamically, regenerate
+   them on zoom or pan using debounce. For now, `moves`
+   simply determines if a given move is legal. */
+
+export abstract class PieceType {
+  abstract whiteImage: HTMLImageElement
+  abstract blackImage: HTMLImageElement
+  /**
+   * Determines whether a certain piece of this type can
+   * move to a certain tile.
+   *
+   * @param piece The piece being moved.
+   * @param to The tile that it may or may not move to.
+   * @returns Whether `piece` may move to the tile `to`.
+   */
+  abstract moves(piece: Piece, to: Vector): boolean
+  takes(piece: Piece, to: Vector): boolean {
+    return this.moves(piece, to)
+  }
+}
+
+export interface Piece {
+  type: PieceType
+  location: Vector
+  color: Color
+}
+
+/**
+ * A shorthand for grabbing images in the /public/images/ folder.
+ */
+function createImage(src: string) {
+  let img = new Image()
+  img.src = `/images/${src}.png`
+  return img
+}
+
+// ------------------- PIECES ---------------------- //
+
+export let king = new (class extends PieceType {
+  whiteImage = createImage("wk")
+  blackImage = createImage("bk")
+  override moves(piece: Piece, to: Vector): boolean {
+    let diff = piece.location.absMinus(to)
+    return (
+      Math.abs(diff.x) <= 1 && Math.abs(diff.y) <= 1 && Math.abs(diff.y) <= 1
+    )
+  }
+})()
+
+export let bishop = new (class extends PieceType {
+  whiteImage = createImage("wb")
+  blackImage = createImage("bb")
+  override moves(piece: Piece, to: Vector): boolean {
+    let diff = piece.location.absMinus(to)
+    return diff.x === diff.y
+  }
+})()
+
+export let rook = new (class extends PieceType {
+  whiteImage = createImage("wr")
+  blackImage = createImage("br")
+  override moves(piece: Piece, to: Vector): boolean {
+    let diff = piece.location.minus(to)
+    return diff.x === 0 || diff.y === 0
+  }
+})()
+
+export let pawn = new (class extends PieceType {
+  whiteImage = createImage("wp")
+  blackImage = createImage("bp")
+  override moves(piece: Piece, to: Vector): boolean {
+    let diff = piece.location.minus(to)
+    return (
+      diff.x === 0 &&
+      (piece.color === "WHITE"
+        ? diff.y === -1 || diff.y === -2
+        : diff.y === 1 || diff.y === 2)
+    )
+  }
+})()
+
+export let knight = new (class extends PieceType {
+  whiteImage = createImage("wn")
+  blackImage = createImage("bn")
+  override moves(piece: Piece, to: Vector): boolean {
+    let diff = piece.location.absMinus(to)
+    return (diff.x === 2 && diff.y === 1) || (diff.x === 1 && diff.y === 2)
+  }
+})()
+
+type Moves = (piece: Piece, to: Vector) => boolean
+
+function combine(m1: Moves, m2: Moves): Moves {
+  return (piece: Piece, to: Vector) => {
+    return m1(piece, to) || m2(piece, to)
+  }
+}
+
+export let queen = new (class extends PieceType {
+  whiteImage = createImage("wq")
+  blackImage = createImage("bq")
+  override moves = combine(rook.moves, bishop.moves)
+})()
+
+/** 
+ * @param d The absolute change in position per "step".
+ * @param n The maximum number of steps; by default, there
+ *          is no maximum.
+ * @param symmetric Should does the order of d.x and d.y
+ *                  matter? This is relevant for pieces
+ *                  like the knight, where we want an L shape
+ *                  in any direction.
+ */
+function step(d: Vector, n: number = Infinity, symmetric: boolean = false): Moves {
+  throw new Error('Not implemented.')
+}
